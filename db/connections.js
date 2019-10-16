8a11316482af0c26377f9dcfc38610d6cdf43a26
vbc221 <vbc221@nyu.edu>
2019-10-15 20:42:36 -0500

    connection changes

--- db/connections.js
+++ db/connections.js
@@ -2,11 +2,12 @@ const mongoose=require('mongoose')
 
 mongoose.connect('mongodb://localhost/recipe')
 
-let mongoURI='if (process.env.NODE_ENV === "production") {
+let mongoURI = "";
+if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/Vincent-Chen-Recipe-App";
-}'
+}
 mongoose.Promise=Promise
 
 module.exports=mongoose 
